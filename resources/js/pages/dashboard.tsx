93b159f25c6bbb3a8af419add52a7a65278a2157
Audrei Louis <a.rapista.538116@umindanao.edu.ph>
2026-06-17 23:06:16 +0800

    static bar chart added in dashbaord

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -2,6 +2,8 @@ import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
+import ChartBarDefault from '@/components/ChartBarDefault'
+
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -25,9 +27,9 @@ export default function Dashboard() {
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
-                </div>
-                <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 rounded-xl border md:min-h-min">
-                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative rounded-xl border">
+                        <ChartBarDefault />
+                    </div>
                 </div>
             </div>
         </AppLayout>
